2e9049c2f36d58620250e3335417dc6117f1e126
Dexter Sampson <dexter1.sampson@gmail.com>
2020-03-08 17:22:05 -0400

    arrays

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$('#start').on('click', function(){
+    $('#subwrapper').remove();
+})
+
+var question = [{
+    question: "Which sports figure holds the record for appearing the most times on the cover of Sports Illustrated?",
+    answers: ["Peyton Manning", "Micheal Jordan", "Tiger Woods", "Lebron James"],
+    correctAnswer: "Micheal Jordan"
+}, {
+    question: "Who is the only man to play both an NFL game and a MLB game in a single day?",
+    answers: ["Bo Jackson", "Barry Bonds", "Tim Tebow", "Deon Sanders"],
+    correctAnswer: "Deon Sanders"
+}, {
+    question: "What is the most watched sport in the world?",
+    answers: ["Baseball", "Basketball","Footabll(Soccer)","Football(American Football)"],
+    correctAnswer: "Footabll(Soccer)"
+}, {
+    question:
+}];
